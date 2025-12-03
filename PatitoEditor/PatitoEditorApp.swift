2bf3691c5a899fce368995fdf07848f2519bdbcc
Santiago Quihui <santiagoquihui@hotmail.com>
2025-12-03 04:16:15 -0600

    Initial Commit

---
+++ PatitoEditor/PatitoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatitoEditorApp.swift
+//  PatitoEditor
+//
+//  Created by Santiago Quihui on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PatitoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
